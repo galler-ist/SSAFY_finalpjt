11517401c07c65e6bb86580decc06f41d22a87c7
김근욱 <nonggeun0827@naver.com>
2024-05-19 14:17:10 +0900

    :white_check_mark: 홈, 로그인, 게시판 조금 구현

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,13 +1,38 @@
 <template>
-  <div>
+  <nav class="navbar navbar-expand-lg bg-body-lg">
+    <div class="container-fluid">
+      <RouterLink class="navbar-brand" style="color : #702828" :to="{ name: 'HomeView' }">Home</RouterLink>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav me-auto">
 
-  </div>
+          <RouterLink class="nav-link" style="color : #702828" :to="{ name: 'ArticleView' }">Articles</RouterLink>
+        </div>
+        <div class="navbar-nav ms-auto">
+          <RouterLink class="nav-link" style="color : #702828" v-if="!isLogin" :to="{ name: 'SignUpView' }">Sign In</RouterLink>
+          <RouterLink class="nav-link" style="color : #702828" v-if="!isLogin" :to="{ name: 'LogInView' }">Log In</RouterLink>
+          <button class="nav-link btn btn-link" style="color : #702828" v-else @click="logOut">Log Out</button>
+        </div>
+      </div>
+    </div>
+  </nav>
+  <RouterView />
 </template>
 
 <script setup>
+import { RouterView, RouterLink } from 'vue-router'
+import { computed } from 'vue'
+import { useCounterStore } from '@/stores/counter.js' 
+
+const counterStore = useCounterStore()
+const isLogin = computed(() => counterStore.isLogin)
+const logOut = () => {
+  counterStore.logOut()
+}
 
 </script>
 
 <style scoped>
-
 </style>
---
+++ front/src/views/LogInView.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="login" style="background-color : #F6F1FF">
+    <div class="card m-auto" style="width: 30rem;">
+      <div class="card-body">
+        <h5 class="card-title loginblank">Log In</h5>
+        <form class="card-text" @submit.prevent="logIn">
+          <div class="idblank">
+            <label for="username">ID&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
+            <input type="text" v-model.trim="username" id="username">
+          </div>
+          <div class="passwordblank">
+            <label for="password">PASSWORD&nbsp;</label>
+            <input type="password" v-model.trim="password" id="password">
+          </div>
+          <input class="btn btn-primary w-100 inputblank" type="submit" value="Log In">
+        </form>
+        <hr>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { useCounterStore } from '@/stores/counter'
+
+const username = ref(null)
+const password = ref(null)
+const store = useCounterStore()
+
+const logIn = function () {
+  const payload = {
+    username: username.value,
+    password: password.value
+  }
+  store.logIn(payload)
+}
+
+</script>
+
+<style>
+.login {
+  text-align: center;
+}
+.loginblank {
+  padding: 2rem;
+  
+}
+.idblank {
+  padding: 1rem;
+}
+.passwordblank {
+  padding-bottom: 1rem;
+}
+.inputblank {
+  padding: 2rem;
+}
+</style>
